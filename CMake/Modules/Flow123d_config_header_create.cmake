86476827605eb6596ed224295770da1a9d51d9f1
x3mSpeedy <jan.hybs@tul.cz>
2015-02-12 17:09:33 +0100

    Initial commit

---
+++ CMake/Modules/Flow123d_config_header_create.cmake
@@ -0,0 +1,18 @@
+# CMake script for creating config header file
+
+# grab git details
+include(${FLOW123D_SOURCE_DIR}/CMake/Modules/Flow123d_git_info.cmake)
+
+# write definitions
+MESSAGE (STATUS "CREATING CONFIG.H")
+
+# append git info to already created tmp file
+FILE (APPEND ${CMAKE_BINARY_DIR}/config.h.tmp "#define _GIT_REVISION_ \"${GIT_DESCRIBE}\"\n")
+FILE (APPEND ${CMAKE_BINARY_DIR}/config.h.tmp "#define _GIT_BRANCH_ \"${GIT_BRANCH}\"\n")
+FILE (APPEND ${CMAKE_BINARY_DIR}/config.h.tmp "#define _GIT_URL_ \"${GIT_URL}\"\n")
+FILE (APPEND ${CMAKE_BINARY_DIR}/config.h.tmp "#define _VERSION_NAME_ \"${GIT_VERSION_FULL}\"\n")
+
+# copy the file to the final header only if the version changes
+# reduces needless rebuilds
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_BINARY_DIR}/config.h.tmp config.h)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_BINARY_DIR}/config.h.tmp)
